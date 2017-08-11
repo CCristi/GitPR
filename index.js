e2bb2e8a46a00c7132da75f85645e62f386e9ba0
CCristi <covali.christian@gmail.com>
2017-08-11 13:06:58 +0300

    Add filter support

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import {MetadataReader} from './lib/MetadataReader';
 import {CommitMessageTemplate} from './lib/CommitMessageTemplate';
 import {PageController} from './lib/PageController';
+import {SimpleTemplateDriver} from './lib/SimpleTemplateDriver';
+import config from './config';
 
 chrome.browserAction.onClicked.addListener(() => {
   chrome.tabs.getSelected(null, function (tab) {
     const controller = new PageController(chrome.tabs, tab.id);
     const reader = new MetadataReader(controller);
+    const templateDriver = new SimpleTemplateDriver();
 
     reader.collect({
       reviewers: {
@@ -13,25 +16,42 @@ chrome.browserAction.onClicked.addListener(() => {
         selector: '.js-issue-sidebar-form .css-truncate',
         mapper: function (element) {
           return Array.from(element.children)
-            .filter(reviewerParagraph => Boolean(reviewerParagraph.querySelector('.text-green')))
-            .map(reviewerParagraph => reviewerParagraph.innerText.trim())
-            .join(', ');
+            .filter(reviewerParagraph => Boolean(reviewerParagraph.querySelector('.octicon-check')))
+            .map(reviewerParagraph => reviewerParagraph.innerText.trim());
         }
       },
       jiraTicket: {
         strategy: 'dom-query',
-        selector: 'a[href^="https://atlassian.net/browse/"]',
+        selector: 'a[href*="atlassian.net/"]',
         mapper: e => e.innerHTML.trim(),
       },
       prNumber: {
         strategy: 'js-eval',
         code: 'document.location.pathname.split("/").pop()',
       },
-    }).then(items => {
-      const template = new CommitMessageTemplate();
-      const commitMessage = template.render(items);
+      mergeTitle: {
+        strategy: 'dom-query',
+        selector: '#merge_title_field',
+        mapper: e => e.value,
+      }
+    }).then(data => {
+      data.reviewers = data.reviewers.map(reviewer => {
+        return config.users[reviewer] || reviewer.toLowerCase();
+      });
 
-      return controller.updateInputValue('#merge_message_field', commitMessage);
+      const commitMessage = new CommitMessageTemplate().render(data);
+      const mergeTitle = templateDriver.renderToString(
+        '{{ title | clear }}',
+        {
+          emojis: config.emojis,
+          title: data.mergeTitle,
+        }
+      );
+
+      return Promise.all([
+        controller.updateInputValue('#merge_message_field', commitMessage),
+        controller.updateInputValue('#merge_title_field', mergeTitle),
+      ]);
     });
   });
 });
